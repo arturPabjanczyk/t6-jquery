aaeb5734cfbe9ae84a7a0570c33044cb8024184a
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-26 07:19:00 +0100

    Insert jQuery datepicker with React

--- src/index.js
+++ src/index.js
@@ -3,21 +3,29 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+class DatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.datepickerContainer = React.createRef();
+  }
+
+  componentDidMount() {
+    window.$(this.datepickerContainer.current).datepicker();
+  }
+
+  render() {
+    return <div ref={this.datepickerContainer}/>
+  }
+}
+
 function App() {
-  return(
+  return (
     <div className="App">
       <h1>Hello jQueryUI</h1>
+      <DatePicker />
     </div>
   )
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
-// eslint-disable-next-line no-undef
-$( function() {
-  // eslint-disable-next-line no-undef
-  $( "#datepicker" ).datepicker({
-    onSelect: date => console.log(date)
-  });
-} );
+ReactDOM.render(<App/>, rootElement);
