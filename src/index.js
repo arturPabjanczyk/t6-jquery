9ec563f3279ca351edaec1bc61ef4ec6acdf696c
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-26 07:03:30 +0100

    Insert pure jQuery datepicker

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 
 function App() {
   return(
     <div className="App">
-      <h1>Hello CodeSandBox</h1>
-      <h2>Start editing to see some magic happens</h2>
+      <h1>Hello jQueryUI</h1>
     </div>
   )
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+// eslint-disable-next-line no-undef
+$( function() {
+  // eslint-disable-next-line no-undef
+  $( "#datepicker" ).datepicker({
+    onSelect: date => console.log(date)
+  });
+} );
