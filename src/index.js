e1933c88470c7b653e5b347faebc9275a02c3fdd
Artur Pabjańczyk <artur@pabjanczyk.pl>
2020-10-27 06:26:04 +0100

    Add function giving information about date

--- src/index.js
+++ src/index.js
@@ -25,6 +25,23 @@ class DatePicker extends React.Component {
   }
 }
 
+function DateDetails({date, format}) {
+  const theDate =  new Date(date);
+  return(
+    <div className="DateDetails">
+      <h2>Fun facts about this date</h2>
+      <ol>
+        <li>The date is: {theDate.toString()}</li>
+        <li>Counting from now ({"XXXX"}, it would be {"6 years ago"}.</li>
+        <li>Next valentine's day ({"XXXX"} will be {"in 3 weeks"}.</li>
+        <li>It does not fall within a leap year.</li>
+        <li>It is a summer day (it's between {"XXXX"} and {"YYYY"}.</li>
+        <li>It is not Programmer's Day ({"XXXX"}).</li>
+      </ol>
+    </div>
+  )
+}
+
 class App extends React.Component {
   state = {
     selectedDate: "02/18/1971"
@@ -40,8 +57,11 @@ class App extends React.Component {
         </h2>
         <DatePicker
           onDateChange={(date => this.setState({selectedDate: date}))}
-          initialDate={this.state.selectedDate}
+          initialDate={selectedDate}
         />
+        {selectedDate? (
+          <DateDetails date={selectedDate} format="MM/DD/YYYY"/>
+        ): null}
       </div>
     )
   }
